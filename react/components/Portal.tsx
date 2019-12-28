39184164b5dcac2e16f192b3008cdbfabd40ca87
erislandio.soares <erislandiosoares21@gmail.com>
2019-12-28 15:23:13 -0300

    adding width and height properties

--- react/components/Portal.tsx
+++ react/components/Portal.tsx
@@ -26,6 +26,8 @@ const Portal: FunctionComponent<Props> = ({
     <div
       style={{
         position: 'fixed',
+        width: '100%',
+        height: '100%',
         top: 0,
         left: 0,
         ...(cover
